4c017ee2086e78dce0a3e0f3e30aaf22b4866561
pmacydna@gmail.com <pmacydna@gmail.com>
2013-01-12 15:38:02 -0500

    String class.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'bundler/setup'
 
 require File.dirname(__FILE__) + '/../lib/stringed.rb'
 
+include Stringed
+include Music
+
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
--- spec/stringed/string_spec.rb
+++ spec/stringed/string_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Stringed::String do
+describe String do
 
   it "should have base note" do
     Stringed::String.new('E2').root_note.should eq Music::Note.new('E2')
@@ -9,6 +9,10 @@ describe Stringed::String do
   it "should know what note is at any fret" do
     Stringed::String.new('C2').fret(4).should eq Music::Note.new('E2')
     Stringed::String.new('C2').fret(5).should eq Music::Note.new('F2' )
+  end
+
+  it "should now what fret to use for a pitch" do
+    pending
   end
 
 end
